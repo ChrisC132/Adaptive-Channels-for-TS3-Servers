b11b2f4d82d7d6dce4b4af251ce868ac61b1a319
Christian <c.costa44@gmx.de>
2020-06-16 21:27:24 +0200

    Alpha 1.0

---
+++ src/main/java/Main.java
@@ -0,0 +1,182 @@
+import com.github.theholywaffle.teamspeak3.TS3Api;
+import com.github.theholywaffle.teamspeak3.TS3Config;
+import com.github.theholywaffle.teamspeak3.TS3Query;
+import com.github.theholywaffle.teamspeak3.api.ChannelProperty;
+import com.github.theholywaffle.teamspeak3.api.event.ChannelCreateEvent;
+import com.github.theholywaffle.teamspeak3.api.event.ChannelDeletedEvent;
+import com.github.theholywaffle.teamspeak3.api.event.ChannelDescriptionEditedEvent;
+import com.github.theholywaffle.teamspeak3.api.event.ChannelEditedEvent;
+import com.github.theholywaffle.teamspeak3.api.event.ChannelMovedEvent;
+import com.github.theholywaffle.teamspeak3.api.event.ChannelPasswordChangedEvent;
+import com.github.theholywaffle.teamspeak3.api.event.ClientJoinEvent;
+import com.github.theholywaffle.teamspeak3.api.event.ClientLeaveEvent;
+import com.github.theholywaffle.teamspeak3.api.event.ClientMovedEvent;
+import com.github.theholywaffle.teamspeak3.api.event.PrivilegeKeyUsedEvent;
+import com.github.theholywaffle.teamspeak3.api.event.ServerEditedEvent;
+import com.github.theholywaffle.teamspeak3.api.event.TS3EventType;
+import com.github.theholywaffle.teamspeak3.api.event.TS3Listener;
+import com.github.theholywaffle.teamspeak3.api.event.TextMessageEvent;
+import com.github.theholywaffle.teamspeak3.api.wrapper.Channel;
+import com.github.theholywaffle.teamspeak3.api.wrapper.Permission;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    private static List<Channel> channelList;
+    private static Map<Integer, List<Integer>> channels;
+    private static TS3Api api;
+
+    private static int getParentID(int sumID, int mainChannel) {
+        return sumID-mainChannel;
+    }
+
+    /**
+     * Updates Channel List.
+     */
+    private static void updateChannelList(){
+        channelList = api.getChannels();
+    }
+
+    /**
+     * Tests in the channelList if given channel is empty.
+     * @param cid Channel ID
+     * @return true: if channel is empty or not existing; else: false
+     */
+    private static boolean ChannelIsEmpty(int cid){
+        for(Channel ch: channelList) {
+            if(ch.getId() == cid){
+                return (ch.getTotalClients() == 0);
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Updates channels.
+     * All empty channels (except of one) of a channel group will be removed.
+     * If a channel group is full a new channel in this group will be generated.
+     */
+    private static void updateChannels(){
+        try{
+            //Delete empty channels
+            for (Map.Entry<Integer, List<Integer>> entry : channels.entrySet()) {
+                for(int i = 1; i < entry.getValue().size()-1; i++) {
+                    if(ChannelIsEmpty(entry.getValue().get(i))) {
+                        System.out.println("Delete " + entry.getValue().get(i));
+                        api.deleteChannel(entry.getValue().get(i));
+                        entry.getValue().remove(i);
+                        i--;
+                    }
+                }
+
+                //Create channel
+                if(!ChannelIsEmpty(entry.getValue().get(0)) && !ChannelIsEmpty(entry.getValue().get(entry.getValue().size()-1))) {
+                    //Copy properties from Main Channel
+                    final Map<ChannelProperty, String> properties = new HashMap<>();
+                    properties.put(ChannelProperty.CHANNEL_FLAG_SEMI_PERMANENT, "1");
+                    properties.put(ChannelProperty.CPID, String.valueOf(getParentID(entry.getKey(), entry.getValue().get(0))));
+                    properties.put(ChannelProperty.CHANNEL_ORDER, String.valueOf(entry.getValue().get(entry.getValue().size()-1)));
+                    properties.put(ChannelProperty.CHANNEL_CODEC_QUALITY, "10");
+
+                    if(api.getChannelInfo(entry.getValue().get(0)).getMaxClients() > 0) {
+                        properties.put(ChannelProperty.CHANNEL_MAXCLIENTS, String.valueOf(api.getChannelInfo(entry.getValue().get(0)).getMaxClients()));
+                        properties.put(ChannelProperty.CHANNEL_FLAG_MAXCLIENTS_UNLIMITED, "0");
+                    }
+
+                    int createID = api.createChannel("┣ \u200B" + api.getChannelInfo(entry.getValue().get(entry.getValue().size()-1)).getName().substring(2), properties);
+
+                    //Copy permissions from main channel
+                    List<Permission> permissions = api.getChannelPermissions(entry.getValue().get(0));
+                    for(Permission elem: permissions) {
+                        api.addChannelPermission(createID, elem.getName(), elem.getValue());
+                    }
+
+                    System.out.println("Create " + createID);
+                    entry.getValue().add(createID);
+                }
+
+                //Delete last channel if main channel is empty
+                if(ChannelIsEmpty(entry.getValue().get(entry.getValue().size()-1)) && ChannelIsEmpty(entry.getValue().get(0)) && entry.getValue().size() > 1) {
+                    System.out.println("Delete " + entry.getValue().get(entry.getValue().size()-1));
+                    api.deleteChannel(entry.getValue().get(entry.getValue().size()-1));
+                    entry.getValue().remove(entry.getValue().size()-1);
+                }
+            }
+        } catch(Exception ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    public static void main(String ... args) {
+        XMLReader xmlReader = new XMLReader();
+
+        if(!xmlReader.validateXMLSchema()) {
+            System.out.println("XML File is not valid");
+            return;
+        }
+
+        channels = xmlReader.getChannels();
+
+        TS3Query query = new TS3Query(new TS3Config().setHost(xmlReader.getTSHost()).setFloodRate(TS3Query.FloodRate.custom(100)).setQueryPort(xmlReader.getPort()));
+        query.connect();
+
+        api = query.getApi();
+        api.login(xmlReader.getUsername(), xmlReader.getPassword());
+        api.setNickname(xmlReader.getBotNickname());
+        api.selectVirtualServerById(1);
+
+        System.out.println("RUNNING");
+
+
+        api.registerEvent(TS3EventType.CHANNEL);
+        api.registerEvent(TS3EventType.SERVER);
+        api.addTS3Listeners(new TS3Listener() {
+            @Override
+            public void onTextMessage(TextMessageEvent e) {}
+
+            @Override
+            public void onServerEdit(ServerEditedEvent e) {}
+
+            @Override
+            public void onClientMoved(ClientMovedEvent e) {
+                updateChannelList();
+                updateChannels();
+            }
+
+            @Override
+            public void onClientLeave(ClientLeaveEvent e) {
+                updateChannelList();
+                updateChannels();
+            }
+
+            @Override
+            public void onClientJoin(ClientJoinEvent e) {
+                updateChannelList();
+                updateChannels();
+            }
+
+            @Override
+            public void onChannelEdit(ChannelEditedEvent e) {}
+
+            @Override
+            public void onChannelDescriptionChanged(ChannelDescriptionEditedEvent e) {}
+
+            @Override
+            public void onChannelCreate(ChannelCreateEvent e) {}
+
+            @Override
+            public void onChannelDeleted(ChannelDeletedEvent e) {}
+
+            @Override
+            public void onChannelMoved(ChannelMovedEvent e) {}
+
+            @Override
+            public void onChannelPasswordChanged(ChannelPasswordChangedEvent e) {}
+
+            @Override
+            public void onPrivilegeKeyUsed(PrivilegeKeyUsedEvent e) {}
+        });
+    }
+}
---
+++ src/main/java/XMLReader.java
@@ -0,0 +1,137 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class XMLReader {
+    private String xmlPath = "src/TSConfig.xml"; //FIXME Path
+
+    /***
+     * Validates the XML File
+     * @return true: if Validated; false; else
+     */
+    public boolean validateXMLSchema(){
+        try {
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(new File("src/TSConfig.xsd"));
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(new File(xmlPath)));
+        } catch (IOException | SAXException ex) {
+            System.err.println("Exception: " + ex.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * get the whole XML File
+     * @return XML file
+     */
+    private NodeList getNodeList() {
+        try {
+            File file = new File(xmlPath);
+
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(file);
+            doc.getDocumentElement().normalize();
+
+            return doc.getElementsByTagName("AdaptiveChannelsConfig");
+        }
+        catch (Exception ex) {
+            System.err.println(ex.getMessage());
+        }
+
+        return null;
+    }
+
+    /**
+     * get TSInfo from the XML File
+     * @return TSInfo
+     */
+    private NodeList getTSInfo() {
+        Element temp = (Element)getNodeList().item(0);
+        return temp.getElementsByTagName("TSInfo");
+    }
+    /**
+     * get ChannelInfo from the XML File
+     * @return ChannelInfo
+     */
+    private NodeList getChannelInfo() {
+        Element temp = (Element)getNodeList().item(0);
+        return temp.getElementsByTagName("ChannelInfo");
+    }
+    /**
+     * get Hostname
+     * @return TeamSpeak Hostname
+     */
+    public String getTSHost() {
+        return ((Element)getTSInfo().item(0)).getElementsByTagName("TSHost").item(0).getTextContent();
+    }
+    /**
+     * get Bot Nickname
+     * @return TeamSpeak Bot Nickname
+     */
+    public String getBotNickname() {
+        return ((Element)getTSInfo().item(0)).getElementsByTagName("BotNickname").item(0).getTextContent();
+    }
+    /**
+     * get Username
+     * @return TeamSpeak Username
+     */
+    public String getUsername() {
+        return ((Element)((Element)getTSInfo().item(0)).getElementsByTagName("LoginInfo").item(0)).getElementsByTagName("Username").item(0).getTextContent();
+    }
+    /**
+     * get Password
+     * @return TeamSpeak Password
+     */
+    public String getPassword() {
+        return ((Element)((Element)getTSInfo().item(0)).getElementsByTagName("LoginInfo").item(0)).getElementsByTagName("Password").item(0).getTextContent();
+    }
+    /**
+     * get Port
+     * @return TeamSpeak Port
+     */
+    public int getPort() {
+        if(((Element)getTSInfo().item(0)).getElementsByTagName("TSPort").getLength() > 0) {
+            return Integer.valueOf(((Element)getTSInfo().item(0)).getElementsByTagName("TSPort").item(0).getTextContent());
+        }
+        return 10011;
+    }
+
+    /**
+     * get Map of all channels
+     * @return HashMap of adaptive Channels
+     */
+    public Map<Integer, List<Integer>> getChannels() {
+        Map<Integer, List<Integer>> channelList = new HashMap<>();
+
+        NodeList channels = ((Element)getChannelInfo().item(0)).getElementsByTagName("Channel");
+
+        for(int i = 0; i < channels.getLength(); i++) {
+            List<Integer> list = new ArrayList<>();
+            int MainChannelID = Integer.parseInt(((Element)channels.item(i)).getElementsByTagName("MainChannelID").item(0).getTextContent());
+            int ParentChannelID = Integer.parseInt(((Element)channels.item(i)).getElementsByTagName("ParentChannelID").item(0).getTextContent());
+            list.add(MainChannelID);
+            channelList.put(ParentChannelID+MainChannelID, list);
+        }
+
+        return channelList;
+    }
+}
